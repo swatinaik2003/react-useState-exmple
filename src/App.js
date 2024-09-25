32464ba031a7c439585308cdb578cc6cba8c51bb
swatinaik2003 <swatinaik2003>
2024-09-25 13:10:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,24 +10,4 @@ function TodoList() {
     setInput('');
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button>Add Todo</button>
-      </form>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-export default TodoList;
-
-
+ 
